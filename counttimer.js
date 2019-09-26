bdba03a3439a72c8d8eb533d0bf936858a3e57b4
Shahab Hashmi <46817090+shahabsama542@users.noreply.github.com>
2019-09-27 01:45:29 +0530

    Update counttimer.js

--- counttimer.js
+++ counttimer.js
@@ -8,7 +8,7 @@ let countDown = new Date('Sep 28, 2019 00:00:00').getTime(),
       let now = new Date().getTime(),
           distance = countDown - now;
 
-      
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
