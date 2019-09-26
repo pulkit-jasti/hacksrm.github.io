615f45dda2d0d323a9d03515e12ca5ce08dada16
Shahab Hashmi <46817090+shahabsama542@users.noreply.github.com>
2019-09-27 02:32:03 +0530

    Update counttimer.js

--- counttimer.js
+++ counttimer.js
@@ -8,11 +8,11 @@ let countDown = new Date('Sep 28, 2019 00:00:00').getTime(),
       let now = new Date().getTime(),
           distance = countDown - now;
 
-        document.getElementById('days').innerText = Math.floor(distance / (day)),
+        
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-      
+        document.getElementById('days').innerText = Math.floor(distance / (day)),      
       //do something later when date is reached
       //if (distance < 0) {
       //  clearInterval(x);
