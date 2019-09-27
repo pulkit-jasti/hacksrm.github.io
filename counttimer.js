c6b485a8a845055acddc7bb474e41c2a69d88539
Shahab Hashmi <46817090+shahabsama542@users.noreply.github.com>
2019-09-28 00:15:10 +0530

    Update counttimer.js

--- counttimer.js
+++ counttimer.js
@@ -1,22 +1,22 @@
-// const second = 1000,
-//       minute = second * 60,
-//       hour = minute * 60,
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
 
-// let countDown = new Date('Sep 28, 2019 00:00:00').getTime(),
-//     x = setInterval(function() {
+let countDown = new Date('Sep 28, 2019 00:00:00').getTime(),
+    x = setInterval(function() {
 
-//       let now = new Date().getTime(),
-//           /*distance = countDown - now;*/
+      let now = new Date().getTime(),
+          /*distance = countDown - now;*/
 
         
-//         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-//         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-//         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second),
-//         document.getElementById('days').innerText = Math.floor(distance / (day)),      
-//       //do something later when date is reached
-//       //if (distance < 0) {
-//       //  clearInterval(x);
-//       //  'IT'S MY BIRTHDAY!;
-//       //}
+        document.getElementById('hours').innerText = 0//Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = 0 //Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = 0//Math.floor((distance % (minute)) / second),
+        document.getElementById('days').innerText = 0//Math.floor(distance / (day)),      
+      //do something later when date is reached
+      //if (distance < 0) {
+      //  clearInterval(x);
+      //  'IT'S MY BIRTHDAY!;
+      //}
 
-//     }, second)
+    }, second)
